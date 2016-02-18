21430d084e8328cac64ec45d80d215f10c49e4dc
Shuaibird <shuaibird@icloud.com>
2016-02-19 00:23:06 +0800

    add compass

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
 var coffee = require('gulp-coffee');
-var concat = require('gulp-concat');
 var browserify = require('gulp-browserify');
+var compass = require('gulp-compass');
+var concat = require('gulp-concat');
+
 
 var coffeeSources = ['components/coffee/tagline.coffee'];
 var jsSources = [
@@ -11,6 +13,7 @@ var jsSources = [
 	'components/scripts/tagline.js',
 	'components/scripts/template.js'
 ];
+var sassSources = ['componets/sass/style.css'];
 
 gulp.task('coffee', function() {
 	gulp.src(coffeeSources)
@@ -24,4 +27,15 @@ gulp.task('js', function() {
 		.pipe(concat('script.js'))
 		.pipe(browserify())
 		.pipe(gulp.dest('builds/development/js'));
+});
+
+gulp.task('compass', function() {
+	gulp.src(sassSources)
+		.pipe(compass({
+			sass: 'components/sass',
+			image: 'builds/development/images',
+			style: 'expanded'
+		})
+			.on('error', gutil.log))
+		.pipe(gulp.dest('builds/development/css'));
 });
