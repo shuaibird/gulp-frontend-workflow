88b1a76d6743e7bb81c5de16c8fef8e8ec93715b
Shuaibird <shuaibird@icloud.com>
2016-02-19 01:22:54 +0800

    add watch to gulp default

--- gulpfile.js
+++ gulpfile.js
@@ -47,4 +47,4 @@ gulp.task('watch', function() {
 	gulp.watch('components/sass/*.scss', ['compass']);
 });
 
-gulp.task('default', ['coffee', 'js', 'compass']);
+gulp.task('default', ['coffee', 'js', 'compass', 'watch']);
