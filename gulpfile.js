27a5ad9b7c96a2f3fc12f83abf541ee6ef9f415f
Shuaibird <shuaibird@icloud.com>
2016-02-18 23:55:44 +0800

    add browserify

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var gutil = require('gulp-util');
 var coffee = require('gulp-coffee');
 var concat = require('gulp-concat');
+var browserify = require('gulp-browserify');
 
 var coffeeSources = ['components/coffee/tagline.coffee'];
 var jsSources = [
@@ -21,5 +22,6 @@ gulp.task('coffee', function() {
 gulp.task('js', function() {
 	gulp.src(jsSources)
 		.pipe(concat('script.js'))
+		.pipe(browserify())
 		.pipe(gulp.dest('builds/development/js'));
 });
