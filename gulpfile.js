7caafd78281796d92cfe0c738e1be87b3e13220c
Shuaibird <shuaibird@icloud.com>
2016-02-18 23:26:01 +0800

    Add CoffeeScript files

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,12 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
+var coffee = require('gulp-coffee');
 
-gulp.task('log', function() {
-	gutil.log('WELL DONE!');
+var coffeeSources = ['components/coffee/tagline.coffee'];
+
+gulp.task('coffee', function() {
+	gulp.src(coffeeSources)
+		.pipe(coffee({bare: true})
+			.on('error', gutil.log))
+		.pipe(gulp.dest('components/scripts'));
 });
